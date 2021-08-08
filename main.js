5cb6391e3240566b039acb0a0024c5629e5c98ea
Dmitry Komkov <drnecromant@gmail.com>
2021-08-08 20:42:13 +0200

    Fixed spaces

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 class VCard extends HTMLElement {
-    constructor() {
-        super();
-        this.heading = "";
-        this.subheading = "";
-    }
+  constructor() {
+    super();
+    this.heading = "";
+    this.subheading = "";
+  }
 
-    connectedCallback() {
-        this.heading = this.getAttribute("heading");
-        this.subheading = this.getAttribute("subheading");
+  connectedCallback() {
+    this.heading = this.getAttribute("heading");
+    this.subheading = this.getAttribute("subheading");
 
-        this.render();
-    }
+    this.render();
+  }
 
-    render() {
-        this.innerHTML = `
-            <div style="text-align: center; font-family: sans-serif">
-                <h1>${this.heading}</h1>
-                <p>${this.subheading}</p>
-            </div>
-        `;
-    }
+  render() {
+    this.innerHTML = `
+      <div style="text-align: center; font-family: sans-serif">
+        <h1>${this.heading}</h1>
+        <p>${this.subheading}</p>
+      </div>
+    `;
+  }
 }
 
 customElements.define("v-card", VCard);
--- server.js
+++ server.js
@@ -4,27 +4,27 @@ let path = require('path');
 
 http.createServer(function (request, response) {
 
-    let filePath = '.' + request.url;
-    if (filePath === './') {
-        filePath = './index.html';
-    }
-    let extname = String(path.extname(filePath)).toLowerCase();
-    let contentType = 'text/html';
-    let mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.svg': 'application/image/svg+xml'
-    };
-    contentType = mimeTypes[extname] || 'application/octet-stream';
+  let filePath = '.' + request.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = 'text/html';
+  let mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg': 'application/image/svg+xml'
+  };
+  contentType = mimeTypes[extname] || 'application/octet-stream';
 
-    fs.readFile(filePath, function(error, content) {
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(content, 'utf-8');
-    });
+  fs.readFile(filePath, function(error, content) {
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(content, 'utf-8');
+  });
 
 }).listen(8125);
 console.log('Server running at http://127.0.0.1:8125/');
