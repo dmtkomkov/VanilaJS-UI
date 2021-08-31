06a822ea7ca2eb04c0a5fce11ef39a822bd5f559
Dmitry Komkov <drnecromant@gmail.com>
2021-08-31 23:36:03 +0200

    Style fixed

--- server.js
+++ server.js
@@ -36,23 +36,23 @@ http.createServer(function (request, response) {
   console.log(contentType);
 
   fs.readFile(filePath, function(error, content) {
-  if (error) {
-    if(error.code === 'ENOENT') {
-      fs.readFile('./404.html', function(error, content) {
-        response.writeHead(404, { 'Content-Type': 'text/html' });
-        response.end(content, 'utf-8');
-      });
+    if (error) {
+      if(error.code === 'ENOENT') {
+        fs.readFile('./404.html', function(error, content) {
+          response.writeHead(404, { 'Content-Type': 'text/html' });
+          response.end(content, 'utf-8');
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+      }
     }
     else {
-      response.writeHead(500);
-      response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
     }
-  }
-  else {
-    response.writeHead(200, { 'Content-Type': contentType });
-    response.end(content, 'utf-8');
-  }
-});
+  });
 
 }).listen(8125);
 console.log('Server running at http://127.0.0.1:8125/');
